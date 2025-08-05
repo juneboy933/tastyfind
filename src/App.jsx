4b9f01081832a2baef09f0f27cbfd9938a8197d1
Brian Oduor <brianoduor03@gmail.com>
2025-08-05 18:34:29 +0300

    Fix case-sensitive import for Searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import SearchBar from './components/searchBar';
 import './App.css'
+import SearchBar from './components/searchBar';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
